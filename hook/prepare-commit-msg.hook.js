2b656dd7db5d925b962a344b7c6b255af926fda1
Radoslaw Mejer <radmen@desmart.com>
2017-07-27 12:08:26 +0200

    Set commit message based on tasks list

---
+++ hook/prepare-commit-msg.hook.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const R = require('ramda');
+const fs = require('fs');
+const promisify = require('es6-promisify');
+const repo = require('../src/repo');
+const log = require('../src/log');
+
+if (process.argv.length > 3) {
+    process.exit(0);
+}
+
+const [msgFile] = process.argv.slice(-1);
+
+const taskToLine = ({ task }, index) => `# ${task} (finished #${index + 1})`;
+
+const buildMessage = tasks => (`
+# Below is list of current tasks.
+#
+# You can use one of them as commit subject.
+# Simply copy selected line to the first line of commit message,
+# and uncomment it.
+#
+# Don't worry about '(finished #N)' postfix, it will be removed.
+# Selected task will be marked as finished.
+#
+${tasks.map(taskToLine).join('\n')}
+`);
+
+const setCommitMessage = async () => {
+    const branch = await repo.getCurrentBranch();
+    const tasks = (await repo.read(branch)).filter(R.propEq('finished', false));
+
+    if (!tasks.length) {
+        process.exit(0);
+    }
+
+    const commitMsg = await promisify(fs.readFile)(msgFile);
+    const body = buildMessage(tasks);
+
+    await promisify(fs.writeFile)(msgFile, `${body}${commitMsg}`);
+    process.exit(0);
+};
+
+setCommitMessage()
+    .catch(err => {
+        log.error(err);
+        process.exit(1);
+    });
