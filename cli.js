6c44d5c5103bd257eef8237412f7cb251fabb2a6
Radoslaw Mejer <radmen@desmart.com>
2017-07-27 10:37:43 +0200

    Determine current branch name

--- cli.js
+++ cli.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs');
+const repo = require('./src/repo');
+const log = require('./src/log');
 
-yargs.commandDir('./src/cmd')
-    .demandCommand()
-    .help()
-    .option('b', {
-        alias: 'branch',
-        default: 'master',
-        describe: 'use selected git branch',
-        type: 'string',
-    })
-    .argv
+const main = async () => {
+    const branch = await repo.getCurrentBranch();
+
+    yargs.commandDir('./src/cmd')
+        .demandCommand()
+        .help()
+        .option('b', {
+            alias: 'branch',
+            default: branch,
+            describe: 'use selected git branch',
+            type: 'string',
+        })
+        .argv
+};
+
+main().catch(log.error);
--- src/repo.js
+++ src/repo.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const promisify = require('es6-promisify');
 const R = require('ramda');
+const { exec } = require('child_process');
 const fsUtils = require('./fs');
 
 const repoFile = fsUtils.getGitRepositoryDir('tasks.json');
@@ -10,22 +11,26 @@ const read = branch => promisify(fs.readFile)(repoFile)
     .then(R.prop(branch))
     .then(R.defaultTo([]))
     .catch(() => []);
-    
+
 const readAll = () => promisify(fs.readFile)(repoFile)
     .then(JSON.parse)
-    .catch(() => {});
+    .catch(() => { });
 
 const write = async (branch, entries) => {
     const items = await readAll();
     const itemsToWrite = Object.assign({}, items, {
         [branch]: entries,
     });
-    
+
     return promisify(fs.writeFile)(repoFile, JSON.stringify(itemsToWrite))
         .then(R.always(itemsToWrite));
 };
 
+const getCurrentBranch = () => promisify(exec)('git rev-parse --abbrev-ref HEAD')
+    .then(R.trim);
+
 module.exports = {
     read,
     write,
+    getCurrentBranch,
 };
